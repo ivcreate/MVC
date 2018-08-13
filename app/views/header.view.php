1b84be85085dbf742537ee07cc6bcf0b278aea87
ivcreate <41729692+ivcreate@users.noreply.github.com>
2018-08-13 15:58:48 +0300

    Create header.view.php

---
+++ app/views/header.view.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap 101 Template</title>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+  </head>
+  <body style="padding-top: 20px;">
+  <div class="container">
